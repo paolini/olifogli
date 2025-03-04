06e69d0b69c6c1fb24d5bbcb06659c5fbfd27ff8
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-03-04 11:21:56 +0100

    refactoring ordering

---
+++ app/components/Ordering.tsx
@@ -0,0 +1,163 @@
+import { useState, Dispatch, SetStateAction } from 'react'
+import { WithId } from 'mongodb'
+import { Row, Data } from '@/app/lib/models'
+
+export type CriterioOrd = {
+  numcampo?: number,
+  nomecampo: string,
+  direzione: number
+}
+
+export type CriterioCerca = {
+  nomecampo: string,
+  value: string
+}
+
+export type Criteria = {
+    criteriCerca: CriterioCerca[],
+    setCriteriCerca: Dispatch<SetStateAction<CriterioCerca[]>>,
+    criteriOrdina: CriterioOrd[],
+    setCriteriOrdina: Dispatch<SetStateAction<CriterioOrd[]>>
+}
+
+const criterioStandard: CriterioOrd = {numcampo: 0, nomecampo: "cognome", direzione: 1}
+const criterioStandard2: CriterioOrd = {numcampo: 1, nomecampo: "nome", direzione: 1}
+
+export function useCriteria(): Criteria {
+    const [criteriCerca, setCriteriCerca] = useState<CriterioCerca[]>([])
+    const [criteriOrdina, setCriteriOrdina] = useState<CriterioOrd[]>([criterioStandard, criterioStandard2])
+    return { criteriCerca, setCriteriCerca, criteriOrdina, setCriteriOrdina }
+}
+
+export function Ordering({ criteria }: { criteria: Criteria }) {
+    return <>
+    <span>Ordinamento per {criteria.criteriOrdina.map(a => a.direzione > 0? a.nomecampo + ":asc" + "  " : a.nomecampo + ":disc").join("  ")}</span>
+    </>
+}
+
+export function CambiaOrdine({ nomecampo, criteria } : { 
+  nomecampo: string,
+  criteria: Criteria
+} ) {
+    return <span style={{cursor: "pointer"}} onClick={() => aggiornaCriteriOrdina(criteria, nomecampo)}>&plusmn;</span>
+}
+
+export function filtraEOrdina(criteria: Criteria, rows: WithId<Row>[]): WithId<Row>[] {
+    return tableOrdina(criteria.criteriOrdina, tableCerca(criteria, rows))
+}
+
+export function InputCerca({field, criteria, size}:{
+  field: string,
+  criteria: Criteria,
+  size?: number
+}) {
+
+    const value = criteria.criteriCerca.filter(crit => crit["nomecampo"] == field).length > 0 ? criteria.criteriCerca.filter(crit => crit["nomecampo"] == field)[0].value : "";
+
+    function Battuta(e: React.ChangeEvent<HTMLInputElement>) {
+        aggiornaCriteriCerca(criteria, field, e.target.value)
+    }
+
+    return <input type="text" size={value == "" ? 1 : value.length + 1} value={value} onChange={Battuta} placeholder="cerca"/>
+}
+
+function tableOrdina(criteriOrdina: CriterioOrd[], rows: WithId<Row>[]): WithId<Row>[] {
+    const rowssort: WithId<Row>[] = [...rows]
+    rowssort.sort((a: WithId<Row>, b: WithId<Row>) => confrontaCriteri(criteriOrdina, a, b))
+//    rowssort.unshift(row0)
+    return (
+      (criteriOrdina.length == 0)? rows : rowssort
+    )
+  }
+
+function confrontaCriteri(criteriOrdina: CriterioOrd[], row1: WithId<Row>, row2: WithId<Row>): number {
+    let i: number = 0
+    let res: number = 0
+
+    while (i < criteriOrdina.length) {
+      res = confronta(criteriOrdina[i].nomecampo, row1.data[criteriOrdina[i].nomecampo] || "", row2.data[criteriOrdina[i].nomecampo] || "")
+      if (! (res == 0)) {
+        return res * criteriOrdina[i].direzione
+      }
+      i++
+    }
+    return res
+  }
+
+function confronta(campo: string, camporow1: string, camporow2: string): number {
+  const campiStringhe: string[] = ["nome", "cognome", "sezione"]
+  const campiNumero: string[] = ["classe", "codice", "punteggio"]
+  const campiData: string[] = ["data_nascita"]
+
+  if (campiStringhe.includes(campo)) {
+    return (
+      (camporow1.toUpperCase() > camporow2.toUpperCase())? 1 :
+        (camporow1.toUpperCase() < camporow2.toUpperCase())?  -1 : 0
+    )
+  }
+  if (campiNumero.includes(campo)) {
+    return (
+      (parseFloat(camporow1) - parseFloat(camporow2) > 0)? 1 :
+        (parseFloat(camporow1) - parseFloat(camporow2) < 0)? -1 : 0
+    )
+  }
+  if (campiData.includes(campo)) {
+    return (
+      (Date.parse(camporow1) > Date.parse(camporow2))? 1 :
+        (Date.parse(camporow1) < Date.parse(camporow2))? -1 : 0
+    )
+  }
+  return 0
+}
+
+function aggiornaCriteriOrdina({criteriOrdina, setCriteriOrdina}: Criteria, nomecampo: string): void {
+    let i: number
+    let cera: boolean = false
+    const critOrdina: CriterioOrd[] = [...criteriOrdina]
+
+    for (i = 0; i < critOrdina.length; i++) {
+      if (critOrdina[i]["nomecampo"] == nomecampo) {
+        cera = true
+        if (critOrdina[i]["direzione"] > 0) {
+          critOrdina[i]["direzione"] = -1
+          setCriteriOrdina([...critOrdina])
+        } else {
+          setCriteriOrdina([...critOrdina.slice(0,i), ...critOrdina.slice(i + 1)])
+        break
+        }
+      }
+    }
+    if (cera == false) {
+      setCriteriOrdina([...critOrdina, {nomecampo: nomecampo, direzione: 1}])
+    }
+  }
+
+function aggiornaCriteriCerca({criteriCerca, setCriteriCerca}:Criteria, nomecampo: string, value: string): void {
+    let i: number
+    let cera: boolean = false
+    const critCerca: CriterioCerca[] = [...criteriCerca]
+    for (i = 0; i < critCerca.length; i++) {
+      if (critCerca[i]["nomecampo"] == nomecampo) {
+        cera = true
+        if (value != "") {
+          critCerca[i]["value"] = value
+          setCriteriCerca([...critCerca])
+          break
+        } else {
+          setCriteriCerca([...critCerca.slice(0,i), ...critCerca.slice(i + 1)])
+        }
+      }
+    }
+    if (cera == false) {
+      setCriteriCerca([...critCerca, {nomecampo: nomecampo, value: value}])
+    }
+  }
+
+function tableCerca({criteriCerca}:Criteria, rows: WithId<Row>[]): WithId<Row>[] {
+    let rowsOk: WithId<Row>[] = [...rows]
+    if (criteriCerca.length >= 0) {
+      criteriCerca.forEach((a) => {rowsOk = rowsOk.filter(riga => (riga.data[a.nomecampo]||'').includes(a.value) )})
+    }
+    return rowsOk
+}
+
--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -5,25 +5,12 @@ import { useQuery, useMutation, StoreObject, gql } from '@apollo/client';
 import { Schema, schemas, AvailableSchemas } from '@/app/lib/schema'
 import { InputCell } from '@/app/components/Input'
 import { Row, Data } from '@/app/lib/models'
+import { Ordering, CambiaOrdine, InputCerca, useCriteria, filtraEOrdina } from '@/app/components/Ordering'
 
 export interface RowWithId extends Row {
     _id: string;
     __typename: string;
 }
-
-type CriterioOrd = {
-  numcampo?: number,
-  nomecampo: string,
-  direzione: number
-}
-
-type CriterioCerca = {
-  nomecampo: string,
-  value: string
-}
-
-const criterioStandard: CriterioOrd = {numcampo: 0, nomecampo: "cognome", direzione: 1}
-const criterioStandard2: CriterioOrd = {numcampo: 1, nomecampo: "nome", direzione: 1}
 
 const GET_ROWS = gql`
   query getRows($sheetId: ObjectId!) {
@@ -68,125 +55,19 @@ export default function Table({sheetId, schemaName}:{sheetId: string, schemaName
   const schema = schemas[schemaName];
   const { loading, error, data } = useQuery<{rows:WithId<Row>[]}>(GET_ROWS, {variables: {sheetId}});
   const [ currentRowId, setCurrentRowId ] = useState<ObjectId|null>(null)
-  const [criteriCerca, setCriteriCerca] = useState<CriterioCerca[]>([])
-  const [criteriOrdina, setCriteriOrdina] = useState<CriterioOrd[]>([criterioStandard, criterioStandard2])
-//  const [inputAttivo, setInputAttivo] = useState<object>({})
+  const criteria = useCriteria()
 
   if (loading) return <div>Loading...</div>
   if (error) return <div>Errore: {error.message}</div>
   if (!data) return [] // cannot really happen
-  const rows = data.rows
-
-  function confronta(campo: string, camporow1: string, camporow2: string): number {
-    const campiStringhe: string[] = ["nome", "cognome", "sezione"]
-    const campiNumero: string[] = ["classe", "codice", "punteggio"]
-    const campiData: string[] = ["data_nascita"]
-
-    if (campiStringhe.includes(campo)) {
-      return (
-        (camporow1.toUpperCase() > camporow2.toUpperCase())? 1 :
-          (camporow1.toUpperCase() < camporow2.toUpperCase())?  -1 : 0
-      )
-    }
-    if (campiNumero.includes(campo)) {
-      return (
-        (parseFloat(camporow1) - parseFloat(camporow2) > 0)? 1 :
-          (parseFloat(camporow1) - parseFloat(camporow2) < 0)? -1 : 0
-      )
-    }
-    if (campiData.includes(campo)) {
-      return (
-        (Date.parse(camporow1) > Date.parse(camporow2))? 1 :
-          (Date.parse(camporow1) < Date.parse(camporow2))? -1 : 0
-      )
-    }
-    return 0
-  }
-  
-
-  function confrontaCriteri(row1: WithId<Row>, row2: WithId<Row>): number {
-    let i: number = 0
-    let res: number = 0
-
-    while (i < criteriOrdina.length) {
-      res = confronta(criteriOrdina[i].nomecampo, row1.data[criteriOrdina[i].nomecampo] || "", row2.data[criteriOrdina[i].nomecampo] || "")
-      if (! (res == 0)) {
-        return res * criteriOrdina[i].direzione
-      }
-      i++
-    }
-    return res
-  }
-
-  function tableOrdina(rows: WithId<Row>[]): WithId<Row>[] {
-    const rowssort: WithId<Row>[] = [...rows]
-    rowssort.sort((a: WithId<Row>, b: WithId<Row>) => confrontaCriteri(a, b))
-//    rowssort.unshift(row0)
-    return (
-      (criteriOrdina.length == 0)? rows : rowssort
-    )
-  }
-
-  function aggiornaCriteriOrdina(nomecampo: string): void {
-    let i: number
-    let cera: boolean = false
-    const critOrdina: CriterioOrd[] = [...criteriOrdina]
-
-    for (i = 0; i < critOrdina.length; i++) {
-      if (critOrdina[i]["nomecampo"] == nomecampo) {
-        cera = true
-        if (critOrdina[i]["direzione"] > 0) {
-          critOrdina[i]["direzione"] = -1
-          setCriteriOrdina([...critOrdina])
-        } else {
-          setCriteriOrdina([...critOrdina.slice(0,i), ...critOrdina.slice(i + 1)])
-        break
-        }
-      }
-    }
-    if (cera == false) {
-      setCriteriOrdina([...critOrdina, {nomecampo: nomecampo, direzione: 1}])
-    }
-  }
-
-  function aggiornaCriteriCerca(nomecampo: string, value: string): void {
-    let i: number
-    let cera: boolean = false
-    const critCerca: CriterioCerca[] = [...criteriCerca]
-    for (i = 0; i < critCerca.length; i++) {
-      if (critCerca[i]["nomecampo"] == nomecampo) {
-        cera = true
-        if (value != "") {
-          critCerca[i]["value"] = value
-          setCriteriCerca([...critCerca])
-          break
-        } else {
-          setCriteriCerca([...critCerca.slice(0,i), ...critCerca.slice(i + 1)])
-        }
-      }
-    }
-    if (cera == false) {
-      setCriteriCerca([...critCerca, {nomecampo: nomecampo, value: value}])
-    }
-  }
-
-  function TableCerca(rows: WithId<Row>[]): WithId<Row>[] {
-    let rowsOk: WithId<Row>[] = [...rows]
-    if (criteriCerca.length >= 0) {
-      criteriCerca.forEach((a) => {rowsOk = rowsOk.filter(riga => (riga.data[a.nomecampo]||'').includes(a.value) )})
-    }
-    return rowsOk
-  }
-
-  const rowsToDisplay: WithId<Row>[] = TableCerca(rows)
-  const rowsDisplay: WithId<Row>[] = tableOrdina(rowsToDisplay)
+  const rows = filtraEOrdina(criteria, data.rows)
 
   return <>
     {/*<pre>{JSON.stringify(inputAttivo.id)}</pre>*/}
     <span>{rows.length} righe</span>
-    {rowsDisplay.length < rows.length && <span>({rowsDisplay.length} visualizzate)</span>}
+    {rows.length < data.rows.length && <span>({rows.length} visualizzate)</span>}
     <br />
-    <span>Ordinamento per {criteriOrdina.map(a => a.direzione > 0? a.nomecampo + ":asc" + "  " : a.nomecampo + ":disc").join("  ")}</span>
+    <Ordering criteria={criteria}/>
     <table>
       <thead>
         <tr>
@@ -195,23 +76,23 @@ export default function Table({sheetId, schemaName}:{sheetId: string, schemaName
               {columnTitle(field)}&nbsp;
                 <CambiaOrdine
                   nomecampo={field}
-                  aggiornaCriteriOrdina={aggiornaCriteriOrdina}
+                  criteria={criteria}
                 />
               </th>)}
         </tr>
         <tr>
             {Object.entries(schema.fields).map(([field,type]) => <th key={"cerca"+field}>
-                { !["ChoiceAnswer", "NumberAnswer", "ScoreAnswer", "Computed"].includes(type) && <InputCerca 
-                  nomecampo={field} 
-                  value={criteriCerca.filter(crit => crit["nomecampo"] == field).length > 0 ? criteriCerca.filter(crit => crit["nomecampo"] == field)[0].value : ""}
-                  aggiornaCriteriCerca={aggiornaCriteriCerca}
-                /> } 
+                { !["ChoiceAnswer", "NumberAnswer", "ScoreAnswer", "Computed"].includes(type) 
+                  && <InputCerca 
+                    field={field} 
+                    criteria={criteria}
+                  /> } 
               </th>)}
           <th>&nbsp;</th>
         </tr>
       </thead>
       <tbody>
-        { rowsDisplay.map((row) => row._id === currentRowId 
+        { rows.map((row) => row._id === currentRowId 
         ? <InputRow sheetId={sheetId} schema={schema} key={row._id.toString()} row={row} done={() => setCurrentRowId(null)}/>
         : <TableRow schema={schema} key={row._id.toString()} row={row} onClick={() => setCurrentRowId(row._id)} />
         )}
@@ -251,35 +132,6 @@ export default function Table({sheetId, schemaName}:{sheetId: string, schemaName
           return field
     }
   }
-}
-
-function InputCerca({size, value, aggiornaCriteriCerca, // setValue, 
-  width, nomecampo}:{
-  //type?: string, 
-  size?: number, 
-  value: string, 
-  width?: string,
-//    setValue?: (value: string) => void,
-  nomecampo: string,
-  aggiornaCriteriCerca: (nomecampo: string, value: string) => void
-}) {
-
-  function Battuta(e: React.ChangeEvent<HTMLInputElement>) {
-    aggiornaCriteriCerca(nomecampo, e.target.value)
-    //setInputAttivo(e.target)
-  }
-
-//    return <><input ref={input => input && input.focus()} type="text" size={value == ""? 1 : value.length + 1} value={value} onChange={Battuta} nomecampo={nomecampo as string} /> </>
-  return <input type="text" size={value == ""? 1 : value.length + 1} value={value} onChange={Battuta} placeholder="cerca"/>
-}
-
-
-
-function CambiaOrdine({ nomecampo, aggiornaCriteriOrdina } : { 
-  nomecampo: string,
-  aggiornaCriteriOrdina: (nomecampo: string) => void
-} ) {
-return <span style={{cursor: "pointer"}} onClick={() => aggiornaCriteriOrdina(nomecampo)}>&plusmn;</span>
 }
 
 function TableRow({schema, row, onClick}: {
