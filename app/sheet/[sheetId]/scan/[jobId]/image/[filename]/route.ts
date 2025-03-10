20273c519a5469168f658423802b53895bb301c8
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-03-10 07:29:47 +0100

    restyling

--- app/sheet/[sheetId]/scan/[jobId]/image/[filename]/route.ts
+++ app/sheet/[sheetId]/scan/[jobId]/image/[filename]/route.ts
@@ -1,5 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { ObjectId } from 'mongodb';
+import { NextResponse } from 'next/server';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
 
