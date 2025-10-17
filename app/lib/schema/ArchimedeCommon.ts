e6ff125c08e4b6bc8c1aebe59d66336bbd87186c
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-10-17 07:44:33 +0200

    bordini ogni 4 risposte vedi issue #2

---
+++ app/lib/schema/ArchimedeCommon.ts
@@ -0,0 +1,29 @@
+import { Field, ChoiceAnswerField, ComputedField } from './fields'
+import Schema from './Schema'
+
+export default class ArchimedeCommon extends Schema {
+    constructor(name: string, description: string) {
+        super(name, description, [
+            new Field('codiceCompito',"codice compito"),
+            new Field('cognome'),
+            new Field('nome'),
+            new Field('dataNascita', 'data di nascita'),
+            new Field('classe'),
+            new Field('sezione'),
+            new Field('scuola'),
+            new ChoiceAnswerField('r01', '1').add_css_style('thick-border-left'),
+            new ChoiceAnswerField('r02', '2'),
+            new ChoiceAnswerField('r03', '3'),
+            new ChoiceAnswerField('r04', '4').add_css_style('thick-border-right'),
+            new ChoiceAnswerField('r05', '5'),
+            new ChoiceAnswerField('r06', '6'),
+            new ChoiceAnswerField('r07', '7'),
+            new ChoiceAnswerField('r08', '8').add_css_style('thick-border-right'),
+            new ChoiceAnswerField('r09', '9'),
+            new ChoiceAnswerField('r10', '10'),
+            new ChoiceAnswerField('r11', '11'),
+            new ChoiceAnswerField('r12', '12').add_css_style('thick-border-right'),
+            new ComputedField('punti').add_css_style('thick-border-right'),
+        ])
+    }
+}
