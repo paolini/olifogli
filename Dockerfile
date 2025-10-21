367a34e69c0a069434d9b70b9b24419f1692d54e
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-10-21 07:03:58 +0200

    fix warnings

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@
 # To push the image:
 # $ docker push paolini/${APP}
 
-FROM node:20-alpine AS base
+FROM node:22-alpine AS base
 
 # Install mongodump
 RUN apk add --no-cache mongodb-tools
--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -15,8 +15,6 @@ const config = {
     databaseName: (process.env.MONGODB_URI?.split('/').pop()?.split('?')[0]) || "olifogli",
 
     options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
-      useUnifiedTopology: true, // removes a deprecating warning when connecting
       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
     }
