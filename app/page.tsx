e321996b17e6a226df34333569dbd0a4012bed85
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-10-29 06:36:21 +0100

    fix breadcrumbs error

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import ApolloProviderClient from '@/app/ApolloProviderClient'
 
 import NavBar from '@/app/components/NavBar' 
 import Splash from '@/app/components/Splash'
+import { BreadcrumbsProvider } from '@/app/components/BreadcrumbsProvider'
 import packageJson from '../package.json'
 
 const version = packageJson.version
 
 export default function Page() {
   return <ApolloProviderClient>
-    <NavBar />
-    <Splash />
+    <BreadcrumbsProvider>
+      <NavBar />
+      <Splash />
+    </BreadcrumbsProvider>
   </ApolloProviderClient>
 }
