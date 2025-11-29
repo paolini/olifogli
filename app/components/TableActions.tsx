e616e7db48b468075cae36eea22ecd0c1c505ce1
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-11-29 14:04:34 +0100

    istruzioni collassate nella tabella

--- app/components/TableActions.tsx
+++ app/components/TableActions.tsx
@@ -28,7 +28,7 @@ export default function TableActions(input: TableActionInput) {
     const ctx = useTableActionsContext(input)
 
     return <>
-        <GlobalMessage className="table-message" title="istruzioni" name="table_instructions" collapsed={false} />
+        <GlobalMessage className="table-message" title="istruzioni" name="table_instructions" collapsed={true} />
         <TableActionsErrors ctx={ctx} />
         <Checkboxes schema={ctx.schema} state={ctx.checkboxesState} setState={ctx.setCheckboxesState} />
         <select
